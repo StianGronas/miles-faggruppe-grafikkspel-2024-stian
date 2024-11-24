e5f7199175242f34dc8876d6b0ed052d8341a76a
Stian Grønås <stian.gronas@miles.no>
2024-11-24 18:49:15 +0100

    More monopoly

--- Godot/monopolkopi/Scripts/Monopol.cs
+++ Godot/monopolkopi/Scripts/Monopol.cs
@@ -1,15 +1,24 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Monopol : Node3D
 {
 
 	private Node3D Player;
 	private int CurrentId = 1;
+	private List<Space> Spaces;
+	private int MaxId;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Player = GetNode("Player") as Node3D;
+		this.Spaces = GetTree().GetNodesInGroup("Spaces")
+			.Select((a) => a is Space ? a as Space : null)
+			.Where((a) => a != null)
+			.ToList();
+		this.MaxId = this.Spaces.Last().Id;
 	}
 
 	private bool IsPressing = false;
@@ -30,13 +39,14 @@ public partial class Monopol : Node3D
 
 	private void MoveToNextSpace(int toMove = 1)
 	{
-		var next = CurrentId + (int) new Random().NextInt64(1, 6); // overflow
+		var next = (CurrentId + (int) new Random().NextInt64(1, 6)) % (MaxId + 1); // overflow
+
 		Space nextNode;
-		if (next == 1) {
-			nextNode = GetNode<Space>("Board/Space");
-		} else {
-			nextNode = GetNode<Space>($"Board/Space{next}");
-		}
+		// Fix %
+		if (next < CurrentId) next++;
+
+		var nextSpace = next == 1 ? "" : next.ToString();
+		nextNode = GetNode<Space>($"Board/Space{nextSpace}");
 		CurrentId = next;
 
 		Player.GlobalPosition = new Vector3(nextNode.GlobalPosition.X, Player.GlobalPosition.Y, nextNode.GlobalPosition.Z);
